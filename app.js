f7540bab82a1f6130b1ce8bd9effdd07b20a17c0
Roy Lin <roylin@fareastone.com.tw>
2022-03-30 17:00:51 +0800

    123

---
+++ app.js
@@ -0,0 +1,9 @@
+const button1 = document.querySelector('button')
+const input1 = document.querySelector('input')
+const list1 = document.querySelector('ul')
+
+function addPlanItem() {
+    console.log("will add a plan here...")
+}
+
+button1.addEventListener('click', addPlanItem)
